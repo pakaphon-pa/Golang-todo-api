dd1de58bbb4a6c6c75fcd58864fa2d0cdc838818
dackmagic115 <pakaphon23@gmail.com>
2022-07-03 17:14:13 +0700

    user role model

--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -1,8 +1,12 @@
 package models
 
 type User struct {
-	ID   uint   `json:"id"`
-	Name string `json:"title"`
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string `json:"-"`
+	RoleId   uint   `json:"role_id"`
+	Roles    []Role `json:"roles" gorm:"foreignKey: RoleId"`
 }
 
 type UserServiceInterface interface {
